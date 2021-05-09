c07c71a0e2898a3f869d11021686a5d333ff95c2
marinaspas <msmarinaspas@gmail.com>
2021-05-09 18:58:28 -0400

    styling the weather app so far

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
+const api = {
+  key: "7730fd97a052c6c91d93f7d161a8c07e",
+  base: "https://api.openweathermap.org/data/2.5",
+};
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="Search ..." />
+        </div>
+      </main>
     </div>
   );
 }
