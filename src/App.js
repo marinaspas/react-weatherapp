1f2547c13fb88b9033884a9c4815a77ca8329491
marinaspas <msmarinaspas@gmail.com>
2021-07-07 16:43:47 -0400

    changed the ui for the weather app and did some refactoring

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import "./App.css";
 import React, { useState } from "react";
 
 const api = {
@@ -16,7 +15,6 @@ function App() {
         .then((res) => res.json())
         .then((result) => {
           setWeather(result);
-          console.log(result, "this is result");
         });
     }
   };
@@ -63,7 +61,7 @@ function App() {
             onChange={(e) => setQuery(e.target.value)}
             value={query}
             className="search-bar"
-            placeholder="Search ..."
+            placeholder="Search a City"
             onKeyPress={search}
           />
         </div>
@@ -72,58 +70,62 @@ function App() {
           <div className="container">
             <div className="location-box">
               <div className="location">
-                {weather.name}, {weather.sys.country}
+                <div className="img-wrap">
+                  <img
+                    src={
+                      "http://openweathermap.org/img/wn/" +
+                      weather.weather[0].icon +
+                      "@2x.png"
+                    }
+                    alt={weather.weather[0].description + " icon"}
+                  />
+                </div>
+                <span className="location-name">
+                  {weather.name}, {weather.sys.country}
+                </span>
               </div>
               <div className="date">{date}</div>
             </div>
+            <button
+              className="metric-btn"
+              onClick={() => {
+                const newType = unitType === "metric" ? "imperial" : "metric";
+                fetch(
+                  `${api.base}weather?q=${query}&units=${newType}&APPID=${api.key}`
+                )
+                  .then((res) => res.json())
+                  .then((result) => {
+                    setWeather(result);
+                    setUnitType(newType);
+                  });
+              }}
+            >
+              {unitType === "metric" ? "imperial" : "metric"}
+            </button>
             <div className="weather-box">
               <div className="temp">
-                {Math.round(weather.main.temp)}&deg; {changeMetricString()}
-              </div>
-              <div className="weather">{weather.weather[0].main}</div>
-              <div>{weather.weather.description}</div>
-              <div>
-                Feels like {Math.round(weather.main.feels_like)}&deg;{" "}
+                {Math.round(weather.main.temp)}
+                <span>&deg;</span>
                 {changeMetricString()}
               </div>
-              <div>Humidity {weather.main.humidity}%</div>
-              <div>Pressure {weather.main.pressure} hPa</div>
-              <div>
-                Min Temp {Math.round(weather.main.temp_min)}&deg;{" "}
-                {changeMetricString()}
+              <div className="weather-details">
+                <p className="weather">{weather.weather[0].description}</p>
+                <p>{weather.weather.description}</p>
+                <p>
+                  Feels like: {Math.round(weather.main.feels_like)}&deg;
+                  {changeMetricString()}
+                </p>
+                <p>Humidity: {weather.main.humidity}%</p>
+                <p>Pressure: {weather.main.pressure} hPa</p>
+                <p>
+                  Min Temp: {Math.round(weather.main.temp_min)}&deg;
+                  {changeMetricString()}
+                </p>
+                <p>
+                  Max Temp: {Math.round(weather.main.temp_max)}&deg;
+                  {changeMetricString()}
+                </p>
               </div>
-              <div>
-                Max Temp {Math.round(weather.main.temp_max)}&deg;{" "}
-                {changeMetricString()}
-              </div>
-              <div>{weather.weather[0].description}</div>
-              <div>
-                <img
-                  src={
-                    "http://openweathermap.org/img/wn/" +
-                    weather.weather[0].icon +
-                    "@4x.png"
-                  }
-                  alt="icon"
-                />
-              </div>
-
-              <button
-                onClick={() => {
-                  const newType = unitType === "metric" ? "imperial" : "metric";
-                  fetch(
-                    `${api.base}weather?q=${query}&units=${newType}&APPID=${api.key}`
-                  )
-                    .then((res) => res.json())
-                    .then((result) => {
-                      setWeather(result);
-                      setUnitType(newType);
-                      console.log(result, "this is result");
-                    });
-                }}
-              >
-                switch to {unitType === "metric" ? "imperial" : "metric"}
-              </button>
             </div>
           </div>
         ) : (
